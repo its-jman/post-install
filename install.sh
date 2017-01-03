3ab323541c0de61c308aab31c5771308208f2640
Josh Manning <jbmmhk@ksu.edu>
2017-01-03 14:21:12 -0600

    Removed replacement of python environment, comment about dynamic chown.

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ apt install -y python-pip python-dev
 pip install setuptools
 pip install wheel
 pip install git+git://github.com/ansible/ansible.git@devel
-# sed -i '1s:.*:#!/usr/bin/python3:' /home/josh/.local/bin/ansible-playbook
 export ANSIBLE_NOCOWS=1
 
 ansible-playbook local-configure.yml
 
+# TODO: This needs to be dynamic
 chown josh:josh .ansible/ -R
 rm -rf ../post-install/
 echo "Done with configuration, removed post-install. "
