e799cc449db9f8aae0cf1d9825848e26f3f67bb9
Josh Manning <jbmmhk@ksu.edu>
2017-11-18 00:57:38 -0600

    updated get python versions to account for alpha/misnamed files/folders

--- library/get_latest_python_versions.py
+++ library/get_latest_python_versions.py
@@ -2,6 +2,8 @@ import requests
 from lxml import html
 from ansible.module_utils.basic import AnsibleModule
 
+PY_URL = 'https://www.python.org/ftp/python/{}/Python-{}.tar.xz'
+
 class Version:
     def __init__(self, version):
         self.version = version
@@ -69,6 +71,21 @@ def _get_matching_major_versions(version, remote_versions):
     return list(matching)
 
 
+def file_exists(location):
+    resp = requests.head(location)
+    return (resp.status_code == 200)
+
+
+def _get_functional_url(versions):
+    for i in range(len(versions) - 1, 0, -1):
+        v = versions[i]
+
+        url = PY_URL.format(v, v)
+        if file_exists(url):
+            return url
+    return ''
+
+
 def main():
     ans_module = AnsibleModule(
         argument_spec = dict(
@@ -85,12 +102,13 @@ def main():
         versions
     )
     matching_versions = map(lambda x: sorted(x), matching_versions)
-    recent_versions = map(lambda x: str(x[-1]), matching_versions)
-    recent_versions = list(recent_versions)
+
+    install_urls = map(lambda x: _get_functional_url(x), matching_versions)
+
 
     ans_module.exit_json(
         changed=True, # Always true. Future, detect currently installed version, and allow for updating.
-        val=recent_versions,
+        val=install_urls,
         log=log,
         initial=versions
     )
